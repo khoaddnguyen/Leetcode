b1503fdd5a771d6f0cdb6c78b3038b42f00cabe8
kwa_laa <khoaddnguyen@gmail.com>
2023-10-07 22:29:13 -0500

    Initial Commit: madlib

---
+++ madlib.py
@@ -0,0 +1,24 @@
+# string concatenation
+noun1 = input("Noun: ")
+number = input("Number: ")
+noun2 = input("Noun: ")
+noun3 = input("Noun: ")
+adjective1 = input("Adjective: ")
+verb1 = input("Verb: ")
+work1 = input("Work: ")
+noun4 = input("Noun: ")
+adjective2 = input("Adjective: ")
+verb2 = input("Verb: ")
+verb3 = input("Verb: ")
+adjective3 = input("Adjective: ")
+
+
+madlib = (f"I decided to go on a vacation to Australia with {noun1}. We got to the airport {number} hours early. \
+When we went through security, I got stopped because I forgot to take {noun2} out of my pocket. \
+We got some JERKY {noun3} for the flight and arrived at the gate. Once we boarded the plane, \
+I was sitting next to a very {adjective1} man. He spent the entire flight {verb1} \
+and talking about his job doing {work1}. Whenever I tried to sleep, he would step around me to \
+go to the {noun4}. I was so {adjective2}. Since I couldn't sleep, I decided to {verb2} and {verb3} instead. \
+Finally, we arrived in Australia. All in all, the flight was {adjective3}!")
+
+print(madlib)
